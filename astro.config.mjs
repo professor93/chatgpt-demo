333ab5d1c8bdf55752f687090e6537534289d46e
Mr.Professor <mronx@ya.ru>
2023-03-10 04:44:18 +0500

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,27 @@
-import { defineConfig } from 'astro/config'
-import unocss from 'unocss/astro'
-import solidJs from '@astrojs/solid-js'
-import vercelDisableBlocks from './plugins/vercelDisableBlocks'
-
-import node from '@astrojs/node'
-import vercel from '@astrojs/vercel/edge'
-
+import { defineConfig } from 'astro/config';
+import unocss from 'unocss/astro';
+import solidJs from '@astrojs/solid-js';
+import vercelDisableBlocks from './plugins/vercelDisableBlocks';
+import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/edge';
+import netlify from "@astrojs/netlify/functions";
 const envAdapter = () => {
   if (process.env.OUTPUT == 'vercel') {
-    return vercel()
+    return vercel();
   } else {
     return node({
       mode: 'standalone'
-    })
+    });
   }
-}
+};
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    unocss(),
-    solidJs()
-  ],
+  integrations: [unocss(), solidJs()],
   output: 'server',
-  adapter: envAdapter(),
+  adapter: netlify(),
   vite: {
-    plugins: [
-      process.env.OUTPUT == 'vercel' && vercelDisableBlocks(),
-    ]
-  },
+    plugins: [process.env.OUTPUT == 'vercel' && vercelDisableBlocks()]
+  }
 });
